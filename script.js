10f58fa21e0f5b387784f88a2e411bbad4a1e691
Frank Kafweku <frankkafweku84@gmail.com>
2023-08-29 12:50:53 +0200

    Declare the varibles to use

---
+++ script.js
@@ -0,0 +1,8 @@
+const day = document.getElementById("day-inp")
+const month = document.getElementById("month-inp")
+const year = document.getElementById("year-inp")
+const submitBtn =  document.getElementById("submit-btn")
+
+
+
+
--- style-guide.md
+++ style-guide.md
@@ -26,7 +26,7 @@ The designs were created to the following widths:
 
 ### Body Copy
 
-- Font size (inputs): 32px
+- Font size (inputs): 
 
 ### Font
 
