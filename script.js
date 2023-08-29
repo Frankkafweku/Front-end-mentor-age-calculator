7451adf61158046f2183d98a85758c672a79fe13
Frank Kafweku <frankkafweku84@gmail.com>
2023-08-29 13:09:20 +0200

    Validated the form to show errors if fields are empty

--- script.js
+++ script.js
@@ -2,7 +2,23 @@ const day = document.getElementById("day-inp")
 const month = document.getElementById("month-inp")
 const year = document.getElementById("year-inp")
 const submitBtn =  document.getElementById("submit-btn")
+const form = document.getElementById("form")
+const dayError = document.getElementById("day-error")
+const monthError = document.getElementById("month-error")
+const yearError = document.getElementById("year-error")
 
+form.addEventListener('submit',(e)=>{
 
+    if(day.value === "" || day.value === null){
+        dayError.innerText="This field is required!!"
+    }
+    if(month.value === "" || month.value === null){
+        monthError.innerText="This field is required!!"
+    }
+    if(year.value === "" || year.value === null){
+        yearError.innerText="This field is required!!"
+    }
+    e.preventDefault();
+})
 
 
